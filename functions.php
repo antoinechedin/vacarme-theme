100e8f153cefe10b947ede91be6124d206f9ec4c
antoinechedin <antoine.chedin@gmail.com>
2025-01-02 17:51:05 +0000

    Worldmap supports hyperlinks

--- functions.php
+++ functions.php
@@ -15,9 +15,29 @@ endif;
 
 function vacarme_scripts()
 {
-    wp_enqueue_style('vacarme',get_stylesheet_uri());
+    wp_enqueue_style('vacarme', get_stylesheet_uri());
     wp_enqueue_style('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css');
     wp_enqueue_script('leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js');
+    wp_enqueue_script('vacarme', get_template_directory_uri() . '/map.js');
+
+    $map_hyperlinks = get_posts(array(
+        'numberposts' => -1,
+        'post_type' => 'map-hyperlink',
+    ));
+    $json_map_hyperlinks = array_map(function ($post) {
+        $geojson = json_decode($post->post_content, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return null;
+        }
+        $geojson['properties']['url'] = get_post_permalink($geojson['properties']['linkedPostId']);
+        return json_encode($geojson);
+    }, $map_hyperlinks);
+
+    wp_add_inline_script(
+        'vacarme',
+        'const geojsonHyperlinks = [' . join(',', $json_map_hyperlinks) . '];',
+        'before'
+    );
 }
 
 add_action('after_setup_theme', 'vacarme_setup');
