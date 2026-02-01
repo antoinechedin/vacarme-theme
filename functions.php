cf9867c7de6e894c48f6c171e8b2abe0bf930f28
antoinechedin <antoine.chedin@gmail.com>
2026-02-01 20:16:48 +0000

    New header and layout

--- functions.php
+++ functions.php
@@ -2,17 +2,18 @@
 if (!function_exists('vacarme_setup')) :
     /**
      * Sets up theme defaults and registers support for various WordPress features.
-     *
-     * Note that this function is hooked into the after_setup_theme hook, which runs
-     * before the init hook.
-     */
+    *
+    * Note that this function is hooked into the after_setup_theme hook, which runs
+    * before the init hook.
+    */
     function vacarme_setup()
     {
         // Add support for block styles.
         add_theme_support('wp-block-styles');
     }
 endif;
-
+add_action('after_setup_theme', 'vacarme_setup');
+    
 function vacarme_scripts()
 {
     wp_enqueue_style('vacarme', get_stylesheet_uri());
@@ -39,6 +40,4 @@ function vacarme_scripts()
         'before'
     );
 }
-
-add_action('after_setup_theme', 'vacarme_setup');
 add_action('wp_enqueue_scripts', 'vacarme_scripts');
